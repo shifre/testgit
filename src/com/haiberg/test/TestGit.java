c75f19c075cf97b9a50387a176a47feedb88e066
xuefen <sxue@haiberg.com>
2014-03-20 15:57:03 +0800

    add this is a test project

--- src/com/haiberg/test/TestGit.java
+++ src/com/haiberg/test/TestGit.java
@@ -2,7 +2,6 @@ package com.haiberg.test;
 
 public class TestGit {
 	public static void main(String[] args) {
-		System.out.println("aa");
-		System.out.println("test git");
+		System.out.println("this is a test project");
 	}
 }
